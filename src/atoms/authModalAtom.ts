18cebac2ddaecbe240136e9797439a3ac9f5ef2b
Shubham Attri <shubham21354@iiitd.ac.in>
2023-07-24 22:46:38 +0530

    updates hero img and added authmodel

--- src/atoms/authModalAtom.ts
+++ src/atoms/authModalAtom.ts
@@ -1,3 +1,5 @@
+// This atom maintains the current state of the authentication modal. It is used to determine whether the modal is open or closed, as well as the type of modal that should be displayed.
+
 import { atom } from "recoil";
 
 type AuthModalState = {
